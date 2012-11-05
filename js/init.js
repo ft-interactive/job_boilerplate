bcdbb3ad2bcb5346e2a2c1c3e23f514bf5c25afa
Peter Feeney <Fighne@hotmail.co.uk>
2012-11-05 10:42:23 +0000

    cleaning house to the basic files needed
    cleaning house to the basic files needed, so only the files required by
    the boilerplate are included.

--- js/init.js
+++
@@ -1,7 +0,0 @@
-var FT = {};
-			FT.interactive = {};
-			FT.interactive.dataset;
-		
-			// add data source URL here
-			FT.interactive.dataSource = 'http://interactive.ftdata.co.uk/data/ft.interactive.data_v2.php?_cf=131&id=181';
-			
