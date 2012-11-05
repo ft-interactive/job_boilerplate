5da2f36e329a4e63a1ce1c171ec0aa4de86f8cf1
Peter Feeney <Fighne@hotmail.co.uk>
2012-11-05 16:04:32 +0000

    redefining core functions
    taken from information supplied by Luke, the FT Labs are/have created a
    message passing library that allows the iframe contained app to change
    parameters of the iFrame.

--- js/_init.js
+++ js/_init.js
@@ -4,6 +4,12 @@
 var FT = FT || {};
 FT.Interactive = FT.Interactive || {
 	iFrameComms: function(){},
-	rdDataSet: function(){},
+	rdDataSet: function(url, jsonPath, iFormat){
+	
+		/*
+			if iFormat is empty automatically assume JSONP
+		*/
+		
+	},
 	wrDataSet: function(){}
 }
