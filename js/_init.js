ffb6c3f3021265bf850291f89e49bdd5a00b1327
Peter Feeney <Fighne@hotmail.co.uk>
2012-11-13 16:19:09 +0000

    id to class change
    id to class change

--- js/_init.js
+++ js/_init.js
@@ -16,12 +16,11 @@ FT.Interactive = FT.Interactive || {
 		if(str == 'SVG' && document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Shape", "1.0")){
 			return true;
 		} else {
-			$('#FTi #content').html('');
-			$('#FTi #content').html('<p>Sorry, your internet browser does not support SVGs, the graphic technology used in this interactive graphic. Most likely you are using an older version of Internet Explorer. </p>');
+			$('#FTi .content').html('');
+			$('#FTi .content').html('<p>Sorry, your internet browser does not support SVGs, the graphic technology used in this interactive graphic. Most likely you are using an older version of Internet Explorer. </p>');
 			setTimeout(function() {
 				 $('#FTi #content').html('<img src="' + url + '">'); 
 			}, 5000);
-			
 		}
 		if(str == 'CANVAS'){
 		}
