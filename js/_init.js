1b0d3236be9ed1322663fe5c9e22a563d8f27c3e
Peter Feeney <Fighne@hotmail.co.uk>
2012-11-14 12:41:18 +0000

    removal of SVG detection code
    removal of the standard SVG code, and

--- js/_init.js
+++ js/_init.js
@@ -36,7 +36,7 @@ FT.Interactive = FT.Interactive || {
 		var rVal = false;
 		switch (str){
 			case 'SVG':
-				
+				// awaiting comment from Luke
 				break;
 			case 'CANVAS':
 				break;
@@ -48,20 +48,5 @@ FT.Interactive = FT.Interactive || {
 		}
 		return rVal;
 	},
-	browserTest: function (str, url){
-		if(str == 'SVG' && document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Shape", "1.0")){
-			return true;
-		} else {
-			$('#FTi .content').html('');
-			$('#FTi .content').html('<p>Sorry, your internet browser does not support SVGs, the graphic technology used in this interactive graphic. Most likely you are using an older version of Internet Explorer. </p>');
-			setTimeout(function() {
-				 $('#FTi #content').html('<img src="' + url + '">'); 
-			}, 5000);
-		}
-		if(str == 'CANVAS'){
-		}
-		if(str == 'CSS3'){
-		}
 	
-	}
 }
